b0050df8176c9a551095da9b27bfd3fa796b2cec
hailey <hailey.murto@gmail.com>
2016-02-17 21:08:44 -0800

    hides previous results when user changes answers.

--- js/scripts.js
+++ js/scripts.js
@@ -8,74 +8,110 @@ $(document).ready(function() {
 
 
     if (temperature === 'hot' && activities === 'beach' && climate === 'ocean') {
+        $("#newyork").hide();
+        $("#iceland").hide();
         $("#turks").show();
       }
 
     if (temperature === 'hot' && activities === 'beach' && climate === 'mountain') {
+        $("#newyork").hide();
+        $("#iceland").hide();
         $("#turks").show();
       }
 
     if (temperature === 'hot' && activities === 'beach' && climate === 'city') {
+        $("#newyork").hide();
+        $("#iceland").hide();
         $("#turks").show();
       }
 
     if (temperature === 'hot' && activities === 'nightlife' && climate === 'ocean') {
+        $("#newyork").hide();
+        $("#iceland").hide();
         $("#turks").show();
       }
 
     if (temperature === 'hot' && activities === 'nightlife' && climate === 'mountain') {
+        $("#turks").hide();
+        $("#iceland").hide();
         $("#newyork").show();
       }
 
     if (temperature === 'hot' && activities === 'nightlife' && climate === 'city') {
+        $("#turks").hide();
+        $("#iceland").hide();
         $("#newyork").show();
       }
 
     if (temperature === 'hot' && activities === 'nature' && climate === 'ocean') {
+        $("#newyork").hide();
+        $("#iceland").hide();
         $("#turks").show();
       }
 
     if (temperature === 'hot' && activities === 'nature' && climate === 'mountain') {
+        $("#newyork").hide();
+        $("#turks").hide();
         $("#iceland").show();
       }
 
     if (temperature === 'hot' && activities === 'nature' && climate === 'city') {
+        $("#newyork").hide();
+        $("#turks").hide();
         $("#iceland").show();
       }
 
     if (temperature === 'cold' && activities === 'beach' && climate === 'ocean') {
+        $("#newyork").hide();
+        $("#iceland").hide();
         $("#turks").show();
       }
 
     if (temperature === 'cold' && activities === 'beach' && climate === 'mountain') {
+        $("#newyork").hide();
+        $("#turks").hide();
         $("#iceland").show();
       }
 
     if (temperature === 'cold' && activities === 'beach' && climate === 'city') {
+        $("#turks").hide();
+        $("#iceland").hide();
         $("#newyork").show();
       }
 
     if (temperature === 'cold' && activities === 'nightlife' && climate === 'ocean') {
+        $("#turks").hide();
+        $("#iceland").hide();
         $("#newyork").show();
       }
 
     if (temperature === 'cold' && activities === 'nightlife' && climate === 'mountain') {
+        $("#turks").hide();
+        $("#iceland").hide();
         $("#newyork").show();
       }
 
     if (temperature === 'cold' && activities === 'nightlife' && climate === 'city') {
+        $("#turks").hide();
+        $("#iceland").hide();
         $("#newyork").show();
       }
 
     if (temperature === 'cold' && activities === 'nature' && climate === 'ocean') {
+        $("#turks").hide();
+        $("#newyork").hide();
         $("#iceland").show();
       }
 
     if (temperature === 'cold' && activities === 'nature' && climate === 'mountain') {
+        $("#turks").hide();
+        $("#newyork").hide();
         $("#iceland").show();
       }
 
     if (temperature === 'cold' && activities === 'nature' && climate === 'city') {
+        $("#turks").hide();
+        $("#newyork").hide();
         $("#iceland").show();
       }
 
